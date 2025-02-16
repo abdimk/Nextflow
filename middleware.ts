261526a13ec5654e9a4837d005675e02cc40719e
Abdisa Merga <abdisamk@gmail.com>
2025-02-17 01:47:17 +0300

    updated1

--- middleware.ts
+++ middleware.ts
@@ -7,22 +7,26 @@
 // };
 
 
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { getAuth, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse, type NextRequest } from "next/server";
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
-export default clerkMiddleware(async (auth, request) => {
-if (!isPublicRoute(request)) {
-await auth.protect()
+export function middleware(req: NextRequest) {
+  if (!isPublicRoute(req)) {
+    const { userId } = getAuth(req);
+    if (!userId) {
+      return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+  }
+  return NextResponse.next();
 }
-})
 
 export const config = {
-matcher: [
-// Skip Next.js internals and all static files, unless found in search params 
-'/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
- // Always run for API routes
- '/(api|trpc)(.*)',
- ],
-}
-
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
